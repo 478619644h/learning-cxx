4008c9ecfc87235d32516ce6a181f1dcd51b9767
yjhuang <478619644@qq.com>
2024-12-23 17:37:35 +0800

    init

---
+++ exercises/12_method_const/main.cpp
@@ -0,0 +1,24 @@
+#include "../exercise.h"
+
+// READ: 有 cv 限定符的成员函数 <https://zh.cppreference.com/w/cpp/language/member_functions>
+
+struct Fibonacci {
+    int numbers[11];
+
+    // Fibonacci(const int (&numbers)[11]){
+    //     for(int i = 0;i < 11;i++) {
+    //         this->numbers[i] = numbers[i];
+    //     }
+    // }
+    // TODO: 修改方法签名和实现，使测试通过
+    int get(int i) const {
+        return numbers[i];
+    }
+};
+
+int main(int argc, char **argv) {
+    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
+    ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
+    std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
+    return 0;
+}
